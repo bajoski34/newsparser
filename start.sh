9ab8029f20eef704ec8c90c028b725984c1654a7
Olaobaju Abraham <olaobajua@gmail.com>
2022-10-01 15:27:02 +0100

    refactor: start.sh

--- start.sh
+++ start.sh
@@ -1,11 +1,11 @@
 #!/bin/sh
 
 # Install app dependencies
-docker-compose run news-app composer install
+docker-compose run app composer install
 #docker-compose run news-node npm install && yarn run dev
 # run migrations
-docker-compose run  news-app php bin/console doctrine:database:create --if-not-exists
-docker-compose run  news-app php bin/console doctrine:migrations:migrate
+docker-compose run  app php bin/console doctrine:database:create --if-not-exists
+docker-compose run  app php bin/console doctrine:migrations:migrate
 
 # start message worker
 docker-compose run --rm app php bin/console messenger:consume async -vv
